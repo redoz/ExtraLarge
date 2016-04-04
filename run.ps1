5d50d537a876f10ef251bc3be42f773f49ac40bd
Patrik <redoz@redoz.com>
2016-04-05 01:37:46 +0200

    Added basic chart support.

--- run.ps1
+++ run.ps1
@@ -1,9 +1,9 @@
 Import-Module -Name "$PSScriptRoot\src\ExtraLarge.psm1" -Force
-cls
+
 $raw = @"
 A,B,C,Date
 2,1,0.5,2016-03-29
-5,10,1,2016-03-29
+5,10,4,2016-03-30
 "@
 
 $data = ConvertFrom-Csv -InputObject $raw
@@ -12,9 +12,10 @@ Remove-Item C:\temp\out3.xlsx -Force
 New-XLFile -Path c:\temp\out3.xlsx -PassThru |
     Add-XLSheet -Name 'Sheet 1' |
         Add-XLTable -Name Table1 -Data $data -Columns @{Name='A';Type=[int]},@{Name='B';Type=[int]},@{Name='C';Type=[float];NumberFormat='Percent'},@{Name='Date';Type=[DateTime]} -PassThru |
-        Add-XLTable -Name Table2 -Data $data -Columns A,B,C,@{Name='Date';Type=[DateTime];NumberFormat=[XLNumberFormat]::Date}  
-
-
+        Add-XLTable -Name Table2 -Data $data -Columns  @{Name='A';Type=[int]},@{Name='B';Type=[int]},@{Name='C';Type=[float]},@{Name='Date';Type=[DateTime];NumberFormat=[XLNumberFormat]::Date} -PassThru |
+        Add-XLChart -Header "Chart 1" -Type "Line" -With { $_ | Add-XLChartSeries -X "Table2[Date]" -Y "Table2[A]" -PassThru | 
+                                                                Add-XLChartSeries -X "Table2[Date]" -Y "Table2[B]" -Type AreaStacked -PassThru |
+                                                                Add-XLChartSeries -X "Table2[Date]" -Y "Table2[C]" -Type AreaStacked -PassThru } -Column 6
 return;
 
 
---
+++ src/Add-XLChart.ps1
@@ -0,0 +1,66 @@
+﻿function Add-XLChart {
+param(
+    [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline=$true)]
+    [OfficeOpenXml.ExcelWorksheet]$Sheet,
+    [string]$Header,
+    [OfficeOpenXml.Drawing.Chart.eChartType]$Type,
+    [int]$Row = 1,
+    [int]$Column = 1,
+    [int]$Width = 800,
+    [int]$Height = 480,
+    [Switch]$PassThru = $false,
+    [Scriptblock]$With = $null
+
+);
+begin {}
+process {
+    $chart = $Sheet.Drawings.AddChart("Chart" + [Guid]::NewGuid().ToString('n'), $Type);
+    $chart.Title.Text = $Header
+    $chart.SetPosition($Row, 0, $Column, 0);
+    $chart.SetSize($Width, $Height);
+
+    if ($With -ne $null) {
+        $null = $chart | ForEach-Object -Process $With
+    }
+    if ($PassThru.IsPresent) {
+        $Sheet;
+    } elseif ($With -eq $null) {
+        $chart;
+    }
+}
+end {}
+}
+
+#TODO add parameter sets so Type can be typed but still optional
+function Add-XLChartSeries {
+param(
+    [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline=$true)]
+    [OfficeOpenXml.Drawing.Chart.ExcelChart]$Chart,
+    [object]$Header = $null,
+    [Parameter(Mandatory = $true)]
+    [string]$XSeries,
+    [Parameter(Mandatory = $true)]
+    [string]$YSeries,
+    [object]$Type = $null,
+    [switch]$PassThru = $false
+)
+begin{}
+process{
+    if ($Type -eq $null) {
+        $series = $Chart.Series.Add($YSeries, $XSeries);
+    } else {
+        $subChart = $Chart.PlotArea.ChartTypes | Where-Object -FilterScript {$_.ChartType -eq [OfficeOpenXml.Drawing.Chart.eChartType]$Type}
+        if ($subChart -eq $null) {
+            $subChart = $Chart.PlotArea.ChartTypes.Add([OfficeOpenXml.Drawing.Chart.eChartType]$Type);
+        }
+        $series = $subChart.Series.Add($YSeries, $XSeries);
+    }
+    if ($Header -ne $null) {
+        $series.Header = [string]$Header;
+    }
+    if ($PassThru.IsPresent) {
+        $Chart
+    }
+}
+end{}
+}
