6d5b5ff569f230ffaba02530f4242861341c819a
Patrik <redoz@redoz.com>
2016-04-05 17:28:53 +0200

    Little bit of docs

--- src/ExtraLarge.psm1
+++ src/ExtraLarge.psm1
@@ -11,4 +11,11 @@ Export-ModuleMember -Function Add-XLTable
 Export-ModuleMember -Function Add-XLChart
 Export-ModuleMember -Function Add-XLChartSeries
 
+<#
+How to generate module manifest:
+New-ModuleManifest -ModuleVersion 0.0.1 -Path .\src\ExtraLarge.psd1 -RootModule ExtraLarge.psm1 -FileList (Get-ChildItem -Path src).Name -RequiredAssemblies "EPPlus.dll" -Author "Patrik Husfloen" -CompanyName "ExtraLarge" -DefaultCommandPrefix "XL" -ProcessorArchitecture MSIL -DotNetFrameworkVersion 3.5 -Tags Excel,EPPlus,Charts -ProjectUri http://github.com/redoz/ExtraLarge -Description "Create Excel files using PowerShell"
+copy C:\dev\vendor\ExtraLarge\src\* -Destination $env:USERPROFILE\Documents\WindowsPowerShell\Modules\ExtraLarge -Container -Recurse
+Publish-Module -WhatIf -Name ExtraLarge -NuGetApiKey <apikey>
+#>
+
               
