695f714416820720ae7f5380ea00b94e8c69531c
Patrik <redoz@redoz.com>
2016-04-05 15:13:34 +0200

    Removed cruft.

--- run.ps1
+++ run.ps1
@@ -10,7 +10,7 @@ $data = ConvertFrom-Csv -InputObject $raw
 Remove-Item C:\temp\out3.xlsx -Force
 
 New-XLFile -Path c:\temp\out3.xlsx -PassThru |
-    Add-XLSheet -Name 'Sheet 1' |# -Columns (Get-Columns -Names A,B,C,Date -Types [int],[int],[float],[DateTime])
+    Add-XLSheet -Name 'Sheet 1' |
         Add-XLTable -Name Table1 -Data $data -Columns @{Name='A';Type=[int]},@{Name='B';Type=[int]},@{Name='C';Type=[float];NumberFormat='Percent'},@{Name='Date';Type=[DateTime]} -PassThru |
         Add-XLTable -Name Table2 -Data $data -Columns  @{Name='A';Type=[int]},@{Name='B';Type=[int]},@{Name='C';Type=[float];Default=30},@{Name='D';Type=[float];Default=99},@{Name='Date';Type=[DateTime];NumberFormat=[XLNumberFormat]::Date} -PassThru |
         Add-XLChart -Header "Chart 1" -Type "Line" -With { $_ | Add-XLChartSeries -X "Table2[Date]" -Y "Table2[A]" -PassThru | 
