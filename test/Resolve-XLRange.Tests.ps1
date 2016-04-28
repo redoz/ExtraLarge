ea206a178f9d9ffa221e0034c244299a437c2a05
Patrik <redoz@redoz.com>
2016-04-28 23:31:57 +0200

    More Resolve-XLRange tests

--- test/Resolve-XLRange.Tests.ps1
+++ test/Resolve-XLRange.Tests.ps1
@@ -84,12 +84,65 @@ Describe "Resolve-XLRange/SheetAndName" {
             [Object]::ReferenceEquals($res.InputObject, $xlSheet) | Should be $true
         }
     }
+    
+    Context "With invalid name scoped to sheet" {
+        $xlSheet = Get-XLSheet -File $xlFile -Name "Sheet1"
+        
+        It "Should throw an exception" {
+            { Resolve-XLRange -Sheet $xlSheet -Name XYZ -Scope 'Sheet' } | Should Throw "Could not resolve range named 'XYZ'"
+        }
+    }
+    
+    Context "With invalid name scoped to file" {
+        $xlSheet = Get-XLSheet -File $xlFile -Name "Sheet1"
+        
+        It "Should throw an exception" {
+            { Resolve-XLRange -Sheet $xlSheet -Name XYZ -Scope File } | Should Throw "Could not resolve range named 'XYZ'"
+        }
+    }
 }
 
 Describe "Resolve-XLRange/SheetAndRC" {
 }
 
 Describe "Resolve-XLRange/FileAndName" {
+    $path = Join-Path $TestDrive 'test.xlsx'
+    Copy-Item -Path .\test\data\WithNamedRange.xlsx -Destination $path
+    $xlFile = Get-XLFile -Path $path 
+    
+    Context "With valid name scoped to sheet" {
+        It "Should throw an exception" {
+            { Resolve-XLRange -File $xlFile -Name Sheet2Scope } | Should Throw "Could not resolve range named 'Sheet2Scope'"
+        }
+    }
+    
+    Context "With valid name scoped to file" {
+        $res = Resolve-XLRange -File $xlFile -Name 'FileScope'
+        
+        It "Range should be an [XLRange]" {
+            $res.Range -is [XLRange] | Should Be $true
+        }
+        
+        It "Returned range should match that of named range" {
+            $res.Range.Range.Start.Address | Should Be "B2"
+            $res.Range.Range.Columns | Should Be 1
+            $res.Range.Range.Rows | Should Be 1
+        }
+        
+        It "Returned range should belong to 'Sheet2'" {
+            $res.Range.Range.Worksheet.Name | Should Be 'Sheet2'
+        }
+        
+        It "InputObject should same as -File parameter" {
+            [Object]::ReferenceEquals($res.InputObject, $xlFile) | Should be $true
+        }
+    }
+    
+    Context "With invalid name" {      
+        It "Should throw an exception" {
+            { Resolve-XLRange -File $xlFile -Name XYZ } | Should Throw "Could not resolve range named 'XYZ'"
+        }
+    }
 }
 
 Describe "Resolve-XLRange/SheetAndAddress" {
