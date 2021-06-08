4bc475af1a05abc08db72102052e7acb51657f14
Patrik <redoz@redoz.com>
2021-06-08 17:50:30 +0200

    Tweak appveyor

--- build/Invoke-AppVeyorTests.ps1
+++ build/Invoke-AppVeyorTests.ps1
@@ -1,3 +1,4 @@
+Import-Module -Name Pester -MinimumVersion 5.2.2
 $res = Invoke-Pester -Path "$PSSCRIPTROOT/../test" -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru -CodeCoverage 'src\\*-XL*.ps1'
 
 (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
